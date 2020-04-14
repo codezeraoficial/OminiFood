3deac0837186ade71bbb042b915c4b212cd3efe2
Leonardo <Leo@DESKTOP-4JKH6AV>
2020-04-13 21:23:51 -0300

    Let's add some cool effects - Scroll Effects

---
+++ resourses/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+
+    /*For Sticky navigation */
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == 'down') {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+        offset: '60px'
+    });
+
+    /*scroll on buttons */
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000);
+    });
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000);
+    });
+
+
+    /*navigation scroll */
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            && location.hostname == this.hostname) {
+
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
